e73de14da28e15d1e7e4ed88f533e985f142a23f
David Rainsford <david.rainsford@acer.edu.au>
2012-07-12 11:23:54 +1000

    Make the proxy run on any work machine in 10.10.0 or 192.168.2 subnet

--- zsh/custom/proxy.zsh
+++ zsh/custom/proxy.zsh
@@ -1,7 +1,7 @@
 alias proxy='export http_proxy=http://192.168.1.248:3128;export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;'
 alias noproxy='export http_proxy='';export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;'
 
-# stuff to run only if I am at work (change 3.28 to your work static IP)
-if [ `ifconfig | grep 10.10.0.28 | wc -l` = 1 ]; then
+# run only if I am at work
+if [ `ifconfig | grep -P '(10.10.0.|192.168.2)' | wc -l` = 1 ]; then
     proxy
 fi
