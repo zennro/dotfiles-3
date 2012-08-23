ca49daee58451ff4ef233eae4fdeae7a7de1f103
David Rainsford <david.rainsford@acer.edu.au>
2012-08-23 16:09:49 +1000

    Don't alias cvs, it's also a hostname!

--- zsh/custom/auto-ssh.zsh
+++ zsh/custom/auto-ssh.zsh
@@ -1,6 +1,13 @@
 local _myhosts
 _myhosts=( ${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[0-9]*}%%\ *}%%,*} )
+_ignore=("cvs");
+
 
 for host in $_myhosts; do
-    alias $host="ssh $host"
+  for command in $_ignore; do
+    if [[ "$host" == "$command" ]] ; then
+      continue 2
+    fi
+  done
+  alias $host="ssh $host"
 done
