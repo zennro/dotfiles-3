b383a9f1d02b0dd9744b91f219423c71352168ad
David Rainsford <david.rainsford@acer.edu.au>
2013-08-01 11:55:04 +1000

    Mounts for //acercentral shares

---
+++ zsh/custom/mounts.zsh
@@ -0,0 +1,3 @@
+alias mount-public='mount -t cifs //acercentral/public /root/acercentral/public/ -o username=acerrainsfordd'
+alias mount-rainsford='mount -t cifs //acercentral/rainsford /root/acercentral/rainsford/ -o username=acerrainsfordd'
+alias mount-software='mount -t cifs //acercentral/software /root/acercentral/software/ -o username=acerrainsfordd'
