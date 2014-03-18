dd164b626de9e9d339313aa51d7e46d5d80fb794
Ionut Andriescu <mysin_7@yahoo.com>
2014-03-18 16:12:53 +0200

    fix proxy

--- zsh/custom/proxy.zsh
+++ zsh/custom/proxy.zsh
@@ -1,9 +1,11 @@
 alias proxy='export http_proxy=http://10.61.9.74:3128;export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;'
 alias noproxy='unset http_proxy;unset HTTPS_PROXY;unset HTTP_PROXY;unset FTP_PROXY;unset https_proxy;unset ftp_proxy'
+alias proxy-git='git config --global http.proxy http://10.61.9.74:3128; git config --global https.proxy http://10.61.9.74:3128'
 
 # run only if I am at work
 if [ `ifconfig | grep 10.61.9 | wc -l` = 1 ]; then
     proxy
+    proxy-git
 fi
 #if [ `ifconfig | grep 192.168.2 | wc -l` = 1 ]; then
 #    proxy
